b693cb8b21438729041414d4bcc84a70d8ba992b
Risha-a <147548634+Risha-a@users.noreply.github.com>
2024-06-17 19:55:38 +0300

    Update phash.py

--- phash.py
+++ phash.py
@@ -28,7 +28,8 @@ def hamming_distance(hash1, hash2):
 
 image_paths = [
   "664e1ea025ae4_noisy_image_1.jpg","noisy_image_1.jpg","photochka_chb.jpg","white.jpg","white_1.jpg","white_2.jpg","white_3.jpg","white_4.jpg","white_5.jpg","white_6.jpg",
-"white_7.jpg","white_8.jpg","white_9.jpg","white_10.jpg","white_11.jpg","white_12.jpg","white_13.jpg","white_14.jpg","white_15.jpg","white_16.jpg","white_17.jpg", "white_18.jpg","white_19.jpg","white_20.jpg","white_21.jpg","white_22.jpg","white_23.jpg","white_24.jpg","white_25.jpg","white_26.jpg","white_27.jpg","white_28.jpg","white_29.jpg",
+"white_7.jpg","white_8.jpg","white_9.jpg","white_10.jpg","white_11.jpg","white_12.jpg","white_13.jpg","white_14.jpg","white_15.jpg","white_16.jpg","white_17.jpg", "white_18.jpg",
+"white_19.jpg","white_20.jpg","white_21.jpg","white_22.jpg","white_23.jpg","white_24.jpg","white_25.jpg","white_26.jpg","white_27.jpg","white_28.jpg","white_29.jpg",
 "white_30.jpg","white_31.jpg","white_32.jpg","white_33.jpg","white_34.jpg","white_35.jpg","white_36.jpg","white_37.jpg","white_38.jpg","white_39.jpg","white_40.jpg",
 "white_41.jpg","white_42.jpg","white_col_4.jpg","white_col_5.jpg","white_col_6.jpg","white_col_7.jpg","white_col_8.jpg","white_col_9.jpg","white_col_10.jpg",
 "white_col_11.jpg","white_col_12.jpg","white_col_13.jpg","white_col_14.jpg","white_col_15.jpg","white_col_16.jpg","white_col_17.jpg","white_col_18.jpg","white_col_19.jpg",
