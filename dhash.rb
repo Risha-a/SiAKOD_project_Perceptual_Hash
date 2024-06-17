515e53a4551be238b869aead379178f5c25ec641
Risha-a <147548634+Risha-a@users.noreply.github.com>
2024-06-17 14:27:45 +0300

    Add files via upload

---
+++ dhash.rb
@@ -0,0 +1,67 @@
+require "vips"
+require "spreadsheet"
+
+module DHashVips
+  def self.bw image
+    (image.has_alpha? ? image.flatten(background: 255) : image).colourspace("b-w")[0]
+  end
+
+  module DHash
+    extend self
+
+    def hamming a, b
+      (a ^ b).to_s(2).count "1"
+    end
+
+    def pixelate input, hash_size
+      DHashVips.bw( if input.is_a? Vips::Image
+        input.thumbnail_image(hash_size + 1, height: hash_size, size: :force)
+      else
+        Vips::Image.thumbnail(input, hash_size + 1, height: hash_size, size: :force)
+      end )
+    end
+
+    def calculate file, hash_size = 8
+      image = pixelate file, hash_size
+      image.cast("int").conv([[1, -1]]).crop(1, 0, hash_size, hash_size).>(0)./(255).cast("uchar").to_a.join.to_i(2)
+    end
+  end
+
+  files = [
+   "664e1ea0193db_noisy_image_1_1.jpg","black_chb.jpg","noisy_image_1_1.jpg","black.jpg","black_1.jpg","black_2.jpg","black_3.jpg","black_4.jpg","black_5.jpg",
+"black_6.jpg","black_7.jpg","black_8.jpg","black_9.jpg","black_10.jpg","black_11.jpg","black_12.jpg","black_13.jpg","black_14.jpg","black_15.jpg","black_16.jpg",
+"black_17.jpg","black_18.jpg","black_19.jpg","black_20.jpg","black_21.jpg","black_22.jpg","black_23.jpg","black_24.jpg","black_25.jpg","black_26.jpg",
+"black_27.jpg","black_28.jpg","black_29.jpg","black_30.jpg","black_31.jpg","black_32.jpg","black_33.jpg","black_34.jpg","black_35.jpg","black_36.jpg",
+"black_37.jpg","black_38.jpg","black_39.jpg","black_40.jpg","black_41.jpg","black_42.jpg","black_col_4.jpg","black_col_5.jpg","black_col_6.jpg",
+"black_col_7.jpg","black_col_8.jpg","black_col_9.jpg","black_col_10.jpg","black_col_11.jpg","black_col_12.jpg","black_col_13.jpg","black_col_14.jpg",
+"black_col_15.jpg","black_col_16.jpg","black_col_17.jpg","black_col_18.jpg","black_col_19.jpg","black_col_20.jpg"
+]
+
+  original_hash = DHash.calculate("black.jpg")
+
+  book = Spreadsheet::Workbook.new
+  sheet1 = book.create_worksheet
+
+  sheet1[0, 0] = "Название фото"
+  sheet1[0, 1] = "Хэш"
+  sheet1[0, 2] = "Время"
+  sheet1[0, 3] = "Хэммингово расстояние"
+
+  row = 1
+
+  files.each do |file|
+    start_time = Time.now
+    hash_value = DHash.calculate(file)
+    end_time = Time.now
+    hashing = hash_value.to_s + "."
+
+    sheet1[row, 0] = file
+    sheet1[row, 1] = hashing
+    sheet1[row, 2] = end_time - start_time
+    sheet1[row, 3] = DHash.hamming(original_hash, hash_value)
+
+    row += 1
+  end
+
+  book.write('black_dhash.xls')
+end
---
+++ idhash.rb
@@ -0,0 +1,80 @@
+require 'ruby-vips'
+require "spreadsheet"
+
+module DHashVips
+  def self.bw image
+    (image.has_alpha? ? image.flatten(background: 255) : image).colourspace("b-w")[0]
+  end
+  module IDHash
+    def self.median array
+      h = array.size / 2
+      return array[h] if array[h] != array[h - 1]
+      right = array.dup
+      left = right.shift h
+      right.shift if right.size > left.size
+      return right.first if left.last != right.first
+      return right.uniq[1] if left.count(left.last) > right.count(right.first)
+      left.last
+    end
+
+    def self.hamming a, b
+      (a ^ b).to_s(2).count "1"
+    end
+
+    def self.calculate(input, power = 3)
+      size = 2 ** power
+      image = if input.is_a? Vips::Image
+        input.thumbnail_image(size, height: size, size: :force)
+      else
+        Vips::Image.thumbnail(input, size, height: size, size: :force)
+      end
+      array = DHashVips.bw(image).to_enum.map &:flatten
+      d1, i1, d2, i2 = [array, array.transpose].flat_map do |a|
+        d = a.zip(a.rotate(1)).flat_map{ |r1, r2| r1.zip(r2).map{ |i, j| i - j } }
+        m = IDHash.median d.map(&:abs).sort
+          [
+            d.map{ |c| c     <  0 ? 1 : 0 }.join.to_i(2),
+            d.map{ |c| c.abs >= m ? 1 : 0 }.join.to_i(2),
+          ]
+      end
+      (((((i1 << size * size) + i2) << size * size) + d1) << size * size) + d2
+    end
+  end
+
+  files = [
+   "red_water.jpg","red_chb.jpg","noisy_image_1_110.jpg","red.jpg","red_1.jpg","red_2.jpg","red_3.jpg","red_4.jpg","red_5.jpg","red_6.jpg","red_7.jpg","red_8.jpg",
+"red_9.jpg","red_10.jpg","red_11.jpg","red_12.jpg","red_13.jpg","red_14.jpg","red_15.jpg","red_16.jpg","red_17.jpg","red_18.jpg","red_19.jpg","red_20.jpg",
+"red_21.jpg","red_22.jpg","red_23.jpg","red_24.jpg","red_25.jpg","red_26.jpg","red_27.jpg","red_28.jpg","red_29.jpg","red_30.jpg","red_31.jpg","red_32.jpg",
+"red_33.jpg","red_34.jpg","red_35.jpg","red_36.jpg","red_37.jpg","red_38.jpg","red_39.jpg","red_40.jpg","red_41.jpg","red_42.jpg","red_col_4.jpg","red_col_5.jpg",
+"red_col_6.jpg","red_col_7.jpg","red_col_8.jpg","red_col_9.jpg","red_col_10.jpg","red_col_11.jpg","red_col_12.jpg","red_col_13.jpg","red_col_14.jpg","red_col_15.jpg",
+"red_col_16.jpg","red_col_17.jpg","red_col_18.jpg","red_col_19.jpg","red_col_20.jpg"
+]
+
+  original_hash = IDHash.calculate("red.jpg")
+
+  book = Spreadsheet::Workbook.new
+  sheet1 = book.create_worksheet
+
+  sheet1[0, 0] = "Название фото"
+  sheet1[0, 1] = "Хэш"
+  sheet1[0, 2] = "Время"
+  sheet1[0, 3] = "Хэммингово расстояние"
+
+  row = 1
+
+  files.each do |file|
+    start_time = Time.now
+    hash_value = IDHash.calculate(file)
+    end_time = Time.now
+    hashing = hash_value.to_s + "."
+
+    sheet1[row, 0] = file
+    sheet1[row, 1] = hashing
+    sheet1[row, 2] = end_time - start_time
+    sheet1[row, 3] = IDHash.hamming(original_hash, hash_value)
+
+    row += 1
+  end
+
+  book.write('red_idhash.xls')
+end
